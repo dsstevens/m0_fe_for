5f7ad4bc3bde05bf422b29151c222733fc45b788
Deanna Stevens <deanna.sofias@gmail.com>
2023-05-17 21:39:46 -0400

    Add challenge 1 & 2

--- for.js
+++ for.js
@@ -4,12 +4,18 @@
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
 
-
+for(var i = 25; i < 75; i++){
+  if (i % 2 ==0)
+  console.log(i);
+}
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+for(var i = 0; i < weather_options.length; i++){
+  console.log("Tomorrow's forecast will be " + weather_options[i])
+}
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
